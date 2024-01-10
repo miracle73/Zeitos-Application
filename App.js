57b64647c3719ca671e9a102a9d5cea4189ddc1b
emmysoft <emmanueloni45@gmail.com>
2024-01-10 03:21:19 -0800

    fix- adjusted welcome page and create account page

--- App.js
+++ App.js
@@ -1,29 +1,49 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import SignUp from './screens/Auth/SignUp';
 
+//import components
+import SignUp from './screens/Auth/SignUp';
+import Welcome from './screens/Welcome';
+import Login from './screens/Auth/Login';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 const Stack = createStackNavigator();
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'DMSans_18pt-Bold.ttf': require('./assets/fonts/DMSans_18pt-Bold.ttf'),
+    'DMSans_18pt-Medium.ttf': require('./assets/fonts/DMSans_18pt-Medium.ttf'),
+    'DMSans_18pt-Light.ttf': require('./assets/fonts/DMSans_18pt-Light.ttf'),
+  });
+
+  useEffect(() => {
+    async function hideSplashScreen() {
+      await SplashScreen.hideAsync();
+    }
+    if (fontsLoaded) {
+      hideSplashScreen();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Create Account" component={SignUp} options={{ headerTitle: "" }} />
+          <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+          <Stack.Screen name="Create Account" component={SignUp} options={{ headerTitle: "", headerStyle: { backgroundColor: "#f5f7ff" } }} />
+          <Stack.Screen name="Login" component={Login} options={{ headerTitle: "" }} />
         </Stack.Navigator>
       </NavigationContainer>
       <StatusBar style='dark' />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/Auth/SignUp.js
+++ screens/Auth/SignUp.js
@@ -1,53 +1,84 @@
 import { View, Text, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import { useState, useEffect } from 'react'
 import tw from 'twrnc';
+import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
+
+//import components
 import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
 
 const SignUp = () => {
 
+    //import navigation
+    const navigation = useNavigation();
+
+    //state management
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleToggle = () => setIsVisible(!isVisible);
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return (
+                    <View style={tw`bg-white rounded-lg m-2 pt-[11px] pb-[10px] pl-[17px] pr-[18px]`}>
+                        <Text
+                            style={tw.style({ fontFamily: 'DMSans_18pt-Medium.ttf' }, 'text-[#141414] text-sm text-center w-[44px]')}>
+                            Log in
+                        </Text>
+                    </View>
+                )
+            }
+        })
+    }, [navigation]);
+
     return (
         <ScrollView>
-            <View style={tw`flex justify-start items-start gap-8 p-4 bg-white h-full`}>
-                <View style={tw`flex justify-start items-start gap-6`}>
-                    <Text style={tw`w-[135px] text-3xl text-black`}>
+            <View style={tw`gap-8 bg-[#F5F7FF] h-full p-4`}>
+                <View style={tw`flex flex-col justify-start items-start gap-6`}>
+                    <Text style={tw.style({ fontFamily: 'DMSans_18pt-Bold.ttf' }, 'w-[135px] text-3xl text-[#141414]')}>
                         Create Account
                     </Text>
-                    <Text style={tw`w-[201px] text-sm text-[#8C8CA1]`}>
-                        Kindly create an Ushift account with either of the options below
+                    <Text style={tw.style({ fontFamily: 'DMSans_18pt-Light.ttf' }, 'w-[201px] text-sm text-[#8C8CA1]')}>
+                        Kindly create an <Text style={tw.style({ fontFamily: 'DMSans_18pt-Bold.ttf' }, 'text-sm text-black')}>Ushift</Text> account with either of the options below
                     </Text>
+                </View>
+                <View style={tw`flex justify-center items-center gap-8 px-4 bg-white rounded-lg`}>
                     <View style={tw`flex flex-col justify-center items-center gap-5`}>
-                        <View style={tw`flex flex-row justify-center items-center gap-8 bg-gray-300 rounded-lg py-5 px-5`}>
+                        <View style={tw`flex flex-row justify-center items-center gap-8 bg-[#f5f7ff] rounded-lg py-5 px-5 m-4`}>
                             <Ionicons name="logo-google" size={24} color="#ff0000" />
-                            <Text style={tw`text-black text-medium text-center`}>Create Account with Google</Text>
+                            <Text style={tw.style({ fontFamily: 'DMSans_18pt-Light.ttf' }, 'text-black text-base text-center')}>Create Account with Google</Text>
                         </View>
-                        <Text style={tw`text-black text-center`}>or Create Account with</Text>
+                        <Text style={tw.style({ fontFamily: 'DMSans_18pt-Light.ttf' }, 'text-black text-center')}>or Create Account with</Text>
                     </View>
                     <View style={tw`flex justify-center items-center gap-8`}>
                         <View style={tw`flex flex-row justify-center items-center gap-6`}>
-                            <CustomInput placeholder="First Name" type="text" value={firstName} style={tw`w-[159px] bg-gray-200 rounded-lg px-4 py-8`} onChangeText={(firstName) => setFirstName(firstName)} />
-                            <CustomInput placeholder="Last Name" type="text" value={lastName} style={tw`w-[159px] bg-gray-200 rounded-lg px-4 py-8`} onChangeText={(lastName) => setLastName(lastName)} />
+                            <CustomInput placeholder="First Name" type="text" value={firstName} style={tw`w-[159px] h-[52px] bg-[#f5f7ff] rounded-lg px-4 ${firstName ? 'border-red-100' : 'border-[#f5f7ff]'}`} onChangeText={(firstName) => setFirstName(firstName)} />
+                            <CustomInput placeholder="Last Name" type="text" value={lastName} style={tw`w-[159px] h-[52px] bg-[#f5f7ff] rounded-lg px-4`} onChangeText={(lastName) => setLastName(lastName)} />
                         </View>
-                        <CustomInput placeholder="Email Address" type="email" value={email} style={tw`w-[326px] bg-gray-200 rounded-lg px-4 py-8`} onChangeText={(text) => setEmail(text)} />
-                        <CustomInput placeholder="Password" type="email" value={password} style={tw`w-[326px] bg-gray-200 rounded-lg px-4 py-8`} onChangeText={(password) => setPassword(password)} />
+                        <CustomInput placeholder="Email Address" type="email" value={email} style={tw`w-[326px] h-[52px] bg-[#f5f7ff] rounded-lg px-4`} onChangeText={(text) => setEmail(text)} />
+
+                        <CustomInput placeholder="Password" type="email" value={password} style={tw`w-[326px] h-[52px] bg-[#f5f7ff] rounded-lg px-4`} onChangeText={(password) => setPassword(password)} secureTextEntry={!isVisible} />
+                        <Ionicons name={isVisible ? "eye" : "eye-off"} size={18} color={"#c4c4c4"} style={tw`absolute bottom-[35px] right-[25px]`} onPress={handleToggle} />
                     </View>
 
                     <View style={tw`flex flex-row justify-center items-center`}>
                         <CustomButton style={tw`bg-[#001c46] rounded-lg py-4 px-8 w-[326px]`}>
-                            <Text style={tw`text-white text-base text-center`}>Create Account</Text>
+                            <Text style={tw.style({ fontFamily: 'DMSans_18pt-Medium.ttf' }, 'text-white text-base text-center')}>Create Account</Text>
                         </CustomButton>
                     </View>
-                    <Text style={tw`text-center text-[#4a4a68]`}>By creating an you agree Ushift Privacy Policy and Terms of service</Text>
+                    <Text style={tw.style({ fontFamily: 'DMSans_18pt-Light.ttf' }, 'text-center text-[#4a4a68]')}>By creating an you agree Ushift Privacy Policy and Terms of service</Text>
+                </View>
 
-                    <View style={tw`flex flex-row justify-between items-center gap-12`}>
-                        <Text style={tw`text-sm text-gray-200`}>Have an account?</Text>
-                        <Text style={tw`text-sm text-[#00c146]`}> Login</Text>
-                    </View>
+                <View style={tw`flex flex-row justify-around items-center`}>
+                    <Text style={tw.style({ fontFamily: 'DMSans_18pt-Light.ttf' }, 'text-sm text-[#8c8ca1]')}>Have an account?</Text>
+                    <Text style={tw.style({ fontFamily: 'DMSans_18pt-Medium.ttf' }, 'text-center text-sm bg-white w-[79px] py-2 text-[#001c46] rounded-lg')}> Login</Text>
                 </View>
             </View>
         </ScrollView>
