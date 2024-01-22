95a6915418931af2109faa2680bfe00badff699f
emmysoft <emmanueloni45@gmail.com>
2024-01-21 17:06:01 -0800

    fixed textinput alignment, settings still in progress

--- screens/Notifications/Notifications.js
+++ screens/Notifications/Notifications.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
+import { Ionicons } from "@expo/vector-icons";
 
 const Notifications = () => {
   return (
     <View style={tw`flex-1 justify-center items-center`}>
-      <Text>Notifications</Text>
+      <Ionicons name="notifications" size={80} color={"#001C46"} />
+      <Text style={tw.style({ fontFamily: 'DMSans_18pt-Medium.ttf' }, 'text-black text-xl')}>Notifications</Text>
     </View>
   )
 }
